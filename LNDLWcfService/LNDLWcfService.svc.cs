4ee37a4c67a574aeeadbc05e0884e8f2ea3e86b1
hpqiang <hpqiang@hotmail.com>
2014-10-30 23:06:36 +0800

    add datepicker and fileName display

--- LNDLWcfService/LNDLWcfService.svc.cs
+++ LNDLWcfService/LNDLWcfService.svc.cs
@@ -23,16 +23,83 @@ namespace LNDLWcfService
         //public List<OrderEntity> getOrderList()
         public List<OrderEntity> getOrderList()
         {
+            List<OrderEntity> l = new List<OrderEntity>();
+
+            var x = from o in db.tblOrder
+                    select o;
+
+            foreach(var y in x)
+            {
+                OrderEntity order = new OrderEntity();
+
+                order.id = y.id;
+                order.productType = y.productType;
+                order.productName = y.productName;
+                order.startDate = y.startDate;
+                order.expectDate = y.expectDate;
+                order.clientPOFilePath = (y.clientPOFile == null || y.clientPOFile == "TBD" || y.clientPOFile == "N/A")? "(TBD)" : y.clientPOFile.ToString().Substring(0, y.clientPOFile.ToString().LastIndexOf('/') - 1);
+                order.clientPOFileName = (y.clientPOFile == null || y.clientPOFile == "TBD" || y.clientPOFile == "N/A") ? "(Empty)" : y.clientPOFile.ToString().Substring(y.clientPOFile.ToString().LastIndexOf('/') + 1, y.clientPOFile.ToString().Length - y.clientPOFile.ToString().LastIndexOf('/') - 1);
+
+                order.vendorPOFilePath = (y.vendorPOFile == null || y.vendorPOFile == "TBD" || y.vendorPOFile == "N/A")? "(TBD)" : y.vendorPOFile.ToString().Substring(0, y.vendorPOFile.ToString().LastIndexOf('/') - 1);
+                order.vendorPOFileName = (y.vendorPOFile == null || y.vendorPOFile == "TBD" || y.vendorPOFile == "N/A") ? "(Empty)" : y.vendorPOFile.ToString().Substring(y.vendorPOFile.ToString().LastIndexOf('/') + 1, y.vendorPOFile.ToString().Length - y.vendorPOFile.ToString().LastIndexOf('/') - 1);
+                l.Add(order);
+            }
+
+            return l;
             //using (Sample db = new Sample())
             //{
                 var query = (from o in db.tblOrder
-                            select new OrderEntity()
+                            select  new OrderEntity()
                             {
                                 id = o.id,
                                 productType = o.productType,
-                                productName = o.productName
-                            }); //.Take(10);
-                //Console.Write("query is "+query.ToList()+"<br />");
+                                productName = o.productName,
+                                startDate = o.startDate,
+                                expectDate = o.expectDate,
+                                clientPOFilePath = o.clientPOFile,// == null ? "(TBD)" : o.clientPOFile.ToString().Substring(0, o.clientPOFile.ToString().LastIndexOf('/')-1),
+                                //clientPOFileName = o.clientPOFile == null ? "(Empty)" : o.clientPOFile.ToString().Substring(o.clientPOFile.ToString().LastIndexOf('/') + 1, o.clientPOFile.ToString().Length),
+                                vendorPOFilePath = o.vendorPOFile // == null ? "(TBD)" : o.vendorPOFile.ToString().Substring(0, o.vendorPOFile.ToString().LastIndexOf('/')),
+                                //vendorPOFileName = o.vendorPOFile == null ? "(Empty)" : o.vendorPOFile.ToString().Substring(o.vendorPOFile.ToString().LastIndexOf('/') + 1, o.vendorPOFile.ToString().Length)
+                            }
+                            ); //.Take(10);
+                foreach (var q in query)
+                {
+                    //q.clientPOFilePath = q.clientPOFilePath.ToString().Substring(q.clientPOFilePath.ToString().LastIndexOf('/'), q.clientPOFilePath.ToString().Length);
+                    //int len = q.clientPOFilePath.ToString().Length;
+                    //string x = q.clientPOFilePath.Substring(0,5);
+                    //Console.Write(q.clientPOFilePath + "\n");
+                    //Console.Write(len + "\n");
+                    if (q.clientPOFilePath != null)
+                    {
+                        string s = q.clientPOFilePath.ToString();
+                        int start = 0;
+                        int end = 0;
+                        if (q.clientPOFilePath.ToString() != "N/A" && q.clientPOFilePath.ToString() != "TBD")
+                        {
+                            start = q.clientPOFilePath.ToString().LastIndexOf('/');
+                            end = q.clientPOFilePath.ToString().Length;
+
+                            q.clientPOFileName = s.Substring(start+1, end-start-1);
+                            Console.WriteLine(q.clientPOFileName + " " + start + " " + end);
+                        }
+                        else
+                            q.clientPOFileName = "Empty";
+
+                        s = q.vendorPOFilePath.ToString();
+                        start = 0;
+                        end = 0;
+                        if (q.vendorPOFilePath.ToString() != "N/A" && q.vendorPOFilePath.ToString() != "TBD")
+                        {
+                            start = q.vendorPOFilePath.ToString().LastIndexOf('/');
+                            end = q.vendorPOFilePath.ToString().Length;
+
+                            q.vendorPOFileName = s.Substring(start+1, end-start-1);
+                            Console.WriteLine(q.vendorPOFileName + " " + start + " " + end);
+                        }
+                        else
+                            q.vendorPOFileName = "Empty";
+                    }
+                }
                 return query.ToList();
             //}
         }
@@ -46,6 +113,10 @@ namespace LNDLWcfService
                 tblorder.id = o.id;
                 tblorder.productType = o.productType;
                 tblorder.productName = o.productName;
+                tblorder.startDate = o.startDate;
+                tblorder.expectDate = o.expectDate;
+                tblorder.clientPOFile = o.clientPOFilePath + o.clientPOFileName;
+                tblorder.vendorPOFile = o.vendorPOFilePath + o.vendorPOFileName;
                 db.Entry(tblorder).State = EntityState.Modified;
                 db.SaveChanges();
             //}
--- LNDLWcfService/OrderEntity.cs
+++ LNDLWcfService/OrderEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,5 +11,18 @@ namespace LNDLWcfService
         public int id { get; set; }
         public string productType { get; set; }
         public string productName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? startDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? expectDate { get; set; }
+        public string clientPOFilePath { get; set; }
+        public string clientPOFileName { get; set; }
+        public string vendorPOFilePath { get; set; }
+        public string vendorPOFileName { get; set; }
+
+        //public string ToShortDateString()
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
